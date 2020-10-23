ee9f7c577725e7cc6f5443a75ed728275b39af17
Eden PolovetS <edenpolovets@gmail.com>
2020-10-23 16:08:40 +0300

    Updates

---
+++ precache-manifest.bc9a2306b2d2faca4fb157a80b0dab05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844bdfdf22c7648438e1f63fb525cf19",
+    "url": "/todo-list-on-react/index.html"
+  },
+  {
+    "revision": "2604c59891a6fe9bd856",
+    "url": "/todo-list-on-react/static/css/main.364c0a63.chunk.css"
+  },
+  {
+    "revision": "c60a7aca91622846cfb8",
+    "url": "/todo-list-on-react/static/js/2.8476a38f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-on-react/static/js/2.8476a38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2604c59891a6fe9bd856",
+    "url": "/todo-list-on-react/static/js/main.acdc14eb.chunk.js"
+  },
+  {
+    "revision": "95a86fdfe016649a4e4e",
+    "url": "/todo-list-on-react/static/js/runtime-main.70470395.js"
+  }
+]);
